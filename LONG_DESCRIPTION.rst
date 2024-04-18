68a376257c7313548238f10334273b5bda9551f5
datawrestler <datawrestler@gmail.com>
2024-04-17 20:23:47 -0400

    setup

---
+++ LONG_DESCRIPTION.rst
@@ -0,0 +1,31 @@
+Official Parcl Labs Bindings for Python
+===================================
+
+A Python library for Parcl Labs API.
+
+
+Setup
+-----
+
+You can install this package by using the pip tool and installing:
+
+    $ pip install parcllabs
+
+Or:
+
+    $ easy_install parcllabs
+
+
+Setting up a Parcl Labs Account
+---------------------------
+
+Sign up for Stripe at https://dashboard.parcllabs.com/signup.
+
+Using the Parcl Labs API
+--------------------
+
+Documentation for the python bindings can be found alongside Stripe's other bindings here:
+
+- https://api.parcllabs.com/docs
+
+In the standard documentation (the first link), most of the reference pages will have examples in Parcl Labs official bindings.
--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,31 @@
+import os
 from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+os.chdir(here)
+
+with open(
+    os.path.join(here, "LONG_DESCRIPTION.rst"), "r", encoding="utf-8"
+) as fp:
+    long_description = fp.read()
+
+version_contents = {}
+with open(os.path.join(here, 'parcllabs', '__version__.py'), 'r', encoding='utf-8') as fp:
+    exec(fp.read(), version_contents)
 
 setup(
     name='parcllabs',
-    version='0.1.0',
+    version=version_contents['VERSION'],
+    description='Python SDK for ParclLabs API',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author='ParclLabs',
+    author_email='team@parcllabs.com',
+    keywords='parcllabs api real estate analytics',
     packages=find_packages(exclude=["tests", "tests.*"]),
-    # package_dir={"": "src"},
+    url='https://github.com/ParclLabs/parcllabs-python',
     install_requires=[
         'requests',
         'pandas'
