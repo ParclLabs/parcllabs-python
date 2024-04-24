e69237fa7c1ba409eb4e5e2936b570fea2fc4178
datawrestler <datawrestler@gmail.com>
2024-04-24 13:11:50 -0400

    signup

--- setup.py
+++ setup.py
@@ -29,6 +29,10 @@ setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11"
     ],
     setup_requires=['wheel']
 )
