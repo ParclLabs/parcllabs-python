5b2ea692f74675ee8a09da49e2809048cc693ff0
datawrestler <datawrestler@gmail.com>
2024-04-24 15:03:56 -0400

    ci/testing

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from unittest.mock import Mock
+from datetime import datetime
+import requests_mock
+from parcllabs import ParclLabsClient
+
+from parcllabs.services.base_service import ParclLabsService  # Adjust the import based on your project structure
+
+# Setup for tests
+@pytest.fixture
+def client_mock():
+    return Mock()
+
+@pytest.fixture
+def service(client_mock):
+    return ParclLabsService(client=client_mock)
+
+import pytest
+import requests
+
+
+@pytest.fixture
+def api_key():
+    return "fake_api_key"
+
+@pytest.fixture
+def client(api_key):
+    return ParclLabsClient(api_key=api_key)
