fab68911069a68a0752eabfd60975cd120637d4c
bhagya <bhagya.sharma@gmail.com>
2024-07-02 12:57:28 -0500

    use regex

--- scripts/copy_readme.py
+++ scripts/copy_readme.py
@@ -1,3 +1,6 @@
+import re
+
+
 FRONT_MATTER = "---\ntitle: Python Library\nslug: python-library-1\n---\n\n"
 
 
@@ -8,21 +11,16 @@ def main():
     input_readme = "README.md"
     output_file = "README_COPY.md"
     with open(input_readme, "r") as file:
-        readme_lines = file.readlines()
+        readme_content = file.read()
 
     # remove links
-    for line in readme_lines:
-        if line.startswith(("![Logo]", "![GitHub Tag]", "![PyPI - Downloads]")):
-            readme_lines.remove(line)
+    readme_content = re.sub("!\[.*]\(.*\)\n", "", readme_content)
 
     # remove irrelavant sections
-    readme_content = "".join(readme_lines)
-    count = 0
     for section in readme_content.split("\n### "):
         if section.startswith("Parcl Labs Data Overview"):
             full_section = "\n### " + section
             readme_content = readme_content.replace(full_section, "")
-        count += 1
 
     with open(output_file, "w") as f:
         f.write(FRONT_MATTER + readme_content)
