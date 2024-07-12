46e08803184262dbc95d8326ecfcafafa831d1e4
bhagya <bhagya.sharma@gmail.com>
2024-07-12 14:44:54 -0500

    retrieve readme category id

--- scripts/copy_readme.py
+++ scripts/copy_readme.py
@@ -1,13 +1,43 @@
+import argparse
+import base64
 import re
+import requests
 
 
-FRONT_MATTER = "---\ntitle: Python Library\nslug: python-library-1\ncategory: 66858eb048f1fd0031734568\n---\n\n"
+FRONT_MATTER = (
+    "---\ntitle: Python Library\nslug: python-library-1\ncategory: CATEGORY_ID\n---\n\n"
+)
+
+
+def get_category_id(rdme_key: str):
+    """
+    Get the category ID for the readme doc
+    """
+    encoded_key = base64.b64encode(f"{rdme_key}:".encode()).decode()
+
+    url = "https://dash.readme.com/api/v1/categories/guides"
+    headers = {"accept": "application/json", f"authorization": f"Basic {encoded_key}"}
+
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise Exception("Failed to get readme category id")
+    else:
+        resp_json = response.json()
+        return resp_json["id"]
 
 
 def main():
     """
     Create custom README copy that adds front matter and deletes irrelevant fields for readme doc deployment
     """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-k", "--key")
+    args = parser.parse_args()
+
+    rdme_key = args.key
+    cat_id = get_category_id(rdme_key)
+    readme_front_matter = FRONT_MATTER.replace("CATEGORY_ID", cat_id)
+
     input_readme = "README.md"
     output_file = "README_COPY.md"
     with open(input_readme, "r") as file:
@@ -22,7 +52,7 @@ def main():
     )
 
     with open(output_file, "w") as f:
-        f.write(FRONT_MATTER + readme_content)
+        f.write(readme_front_matter + readme_content)
 
 
 if __name__ == "__main__":
