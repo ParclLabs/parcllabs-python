ed532e574c9962c9bd3f05bd2f3e45f3ef380a78
zhibindai26 <11509096+zhibindai26@users.noreply.github.com>
2025-01-31 14:56:43 -0600

    cleanup

--- parcllabs/common.py
+++ parcllabs/common.py
@@ -8,73 +8,15 @@ VALID_PORTFOLIO_SIZES = get_enum_values(PortfolioSizes)
 VALID_LOCATION_TYPES = get_enum_values(LocationTypes)
 VALID_US_REGIONS = get_enum_values(USRegions)
 VALID_US_STATE_ABBREV = get_enum_values(USStateAbbreviations)
-
-VALID_US_STATE_FIPS_CODES = [
-    "01",
-    "02",
-    "04",
-    "05",
-    "06",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-    "13",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23",
-    "24",
-    "25",
-    "26",
-    "27",
-    "28",
-    "29",
-    "30",
-    "31",
-    "32",
-    "33",
-    "34",
-    "35",
-    "36",
-    "37",
-    "38",
-    "39",
-    "40",
-    "41",
-    "42",
-    "44",
-    "45",
-    "46",
-    "47",
-    "48",
-    "49",
-    "50",
-    "51",
-    "53",
-    "54",
-    "55",
-    "56",
-    "60",
-    "66",
-    "69",
-    "72",
-    "78",
-    "ALL",
-]
-
+VALID_US_STATE_FIPS_CODES = get_enum_values(USStateFIPSCodes)
 VALID_SORT_BY = get_enum_values(SortByParams)
 VALID_SORT_ORDER = get_enum_values(SortOrder)
 VALID_EVENT_TYPES = get_enum_values(EventTypes)
 
 ID_COLUMNS = [ResponseColumns.PARCL_ID.value, ResponseColumns.PARCL_PROPERTY_ID.value]
+
 DATE_COLUMNS = [ResponseColumns.DATE.value, ResponseColumns.EVENT_DATE.value]
+
 DELETE_FROM_OUTPUT = [
     ResponseColumns.TOTAL.value,
     ResponseColumns.LIMIT.value,
@@ -84,7 +26,6 @@ DELETE_FROM_OUTPUT = [
 ]
 
 PARCL_LABS_DASHBOARD_URL = "https://dashboard.parcllabs.com/signup"
-
 NO_API_KEY_ERROR = (
     f"API Key is required. Please visit {PARCL_LABS_DASHBOARD_URL} to get an API key."
 )
--- parcllabs/enums.py
+++ parcllabs/enums.py
@@ -164,7 +164,67 @@ class USStateAbbreviations(Enum):
     ALL = "ALL"
 
 
-def get_enum_values(enum_class) -> List[Any]:
+class USStateFIPSCodes(Enum):
+    ALABAMA = "01"
+    ALASKA = "02"
+    ARIZONA = "04"
+    ARKANSAS = "05"
+    CALIFORNIA = "06"
+    COLORADO = "08"
+    CONNECTICUT = "09"
+    DELAWARE = "10"
+    DISTRICT_OF_COLUMBIA = "11"
+    FLORIDA = "12"
+    GEORGIA = "13"
+    HAWAII = "15"
+    IDAHO = "16"
+    ILLINOIS = "17"
+    INDIANA = "18"
+    IOWA = "19"
+    KANSAS = "20"
+    KENTUCKY = "21"
+    LOUISIANA = "22"
+    MAINE = "23"
+    MARYLAND = "24"
+    MASSACHUSETTS = "25"
+    MICHIGAN = "26"
+    MINNESOTA = "27"
+    MISSISSIPPI = "28"
+    MISSOURI = "29"
+    MONTANA = "30"
+    NEBRASKA = "31"
+    NEVADA = "32"
+    NEW_HAMPSHIRE = "33"
+    NEW_JERSEY = "34"
+    NEW_MEXICO = "35"
+    NEW_YORK = "36"
+    NORTH_CAROLINA = "37"
+    NORTH_DAKOTA = "38"
+    OHIO = "39"
+    OKLAHOMA = "40"
+    OREGON = "41"
+    PENNSYLVANIA = "42"
+    RHODE_ISLAND = "44"
+    SOUTH_CAROLINA = "45"
+    SOUTH_DAKOTA = "46"
+    TENNESSEE = "47"
+    TEXAS = "48"
+    UTAH = "49"
+    VERMONT = "50"
+    VIRGINIA = "51"
+    WASHINGTON = "53"
+    WEST_VIRGINIA = "54"
+    WISCONSIN = "55"
+    WYOMING = "56"
+    AMERICAN_SAMOA = "60"
+    GUAM = "66"
+    NORTHERN_MARIANA_ISLANDS = "69"
+    PUERTO_RICO = "72"
+    US_VIRGIN_ISLANDS = "78"
+    ALL = "ALL"
+
+
+def get_enum_values(enum_class: Enum) -> List[Any]:
     """
     Get the values of an enum class.
 
@@ -172,7 +232,8 @@ def get_enum_values(enum_class) -> List[Any]:
         enum_class (Enum): The enum class to get the values of.
 
     Returns:
-        list: A list of the values of the enum class.
+        List[Any]: A list of the values of the enum class.
     """
+
     values = [item.value for item in enum_class]
     return values
