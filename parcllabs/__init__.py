fe596d862e0ff1d9a50c825eb159bbe4ba4dbb9c
zhibindai26 <11509096+zhibindai26@users.noreply.github.com>
2025-08-04 11:06:22 -0500

    revert api

--- parcllabs/__init__.py
+++ parcllabs/__init__.py
@@ -1,8 +1,7 @@
 from parcllabs.__version__ import VERSION as __version__  # noqa: F401, N811
 
 # Constants
-# DEFAULT_API_BASE = "https://api.parcllabs.com"
-DEFAULT_API_BASE = "https://api.dev.parcllabs.com"
+DEFAULT_API_BASE = "https://api.parcllabs.com"
 
 api_key: str | None = None
 api_base = DEFAULT_API_BASE
